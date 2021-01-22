53b5f069d23d09c37d3f7f5a928d844d446f5cc4
Ana Gelez <ana@gelez.xyz>
2021-01-22 18:42:30 +0100

    Nix: more complete flake

--- flake.nix
+++ flake.nix
@@ -27,11 +27,9 @@
           ];
         };
     {
-
-      packages.${system} = {
+      packages = {
         # Discord bot
-        wartid-server-discord-bot =
-        pkgs.buildRustCrate {
+        wartid-server-discord-bot = pkgs.buildRustCrate {
           crateName = "wartid-server-discord-bot";
           pname = "wartid-server-discord-bot";
           version = "0.1.0";
@@ -46,12 +44,12 @@
         };
 
         # Server
-        wartid-server =
-        pkgs.buildRustCrate {
+        wartid-server = pkgs.buildRustCrate {
           crateName = "wartid-server";
           pname = "wartid-server";
           version = "0.1.0";
           src = ./.;
+          features = [ "discord-bot" ];
           workspace_member = ./wartid-server;
           cargoSha256 = "sha256-olL7mTcuWQ6d46WIBW8vK3DfsNGbgGBuWQnazALOkk8=";
           meta = with lib; {
@@ -62,7 +60,7 @@
         };
       };
 
-      defaultPackage.${system} = self.packages.${system}.wartid-server;
+      defaultPackage = self.packages.${system}.wartid-server;
       devShell = nixpackages.mkShell {
         name = "wartid";
         buildInputs = with nixpackages; [ rustChannel.rust diesel-cli postgresql ];
@@ -85,6 +83,92 @@
               pg_ctl start -l $LOG_PATH -o "-c listen_addresses= -k $PGHOST"
             fi
         '';
+      };
+    }) // (with nixpkgs.lib; {
+      nixosModule = { config, pkgs }:
+        let
+          cfg = config.services.wartid;
+        in {
+          options.services.wartid = {
+            enable = mkEnableOption "WartID server";
+            enableDiscordBot = mkEnableOption "WartID Discord bot";
+            db = {
+              autoCreate = mkOption {
+                type = types.bool;
+                default = true;
+                description = ''
+                  true if you want NixOS to handle the creation of the database for you, false if you want to do it manually.
+                  In either case, you will need to enable services.postgres
+                '';
+              };
+              user = mkOption {
+                type = types.string;
+                default = "wartid";
+                description = "The database user";
+              };
+              password = mkOption {
+                type = types.string;
+                description = "The database password";
+              };
+              name = mkOption {
+                type = types.string;
+                default = "wartid";
+                description = "The database name";
+              };
+            };
+            discordToken = mkOption {
+              type = types.string;
+              description = "The Discord token for the bot.";
+            };
+            discordAllowedGuilds = mkOption {
+              type = types.listOf types.int;
+              description = "Snowflake IDs of Guilds the bot accepts people from.";
+            };
+            port = mkOption {
+              type = types.int;
+              default = 7878;
+              description = "HTTP listen port";
+            };
+          };
+
+          config = mkIf cfg.enable {
+            systemd.tmpfiles.rules = [
+              "d /tmp/wartid/ - wartid wartid - -"
+            ];
+            users.users.wartid = {
+              group = "wartid";
+            };
+            systemd.services.wartid-server = {
+              description = "WartID server";
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+              environment = {
+                DISCORD_KEY_FILE = "/tmp/wartid/discord_jwt.key";
+                DATABASE_URL = "postgres://${cfg.db.user}:${cfg.db.password}@localhost/${cfg.db.name}";
+                HTTP_BASE_URL = "http://localhost:${cfg.port}";
+              };
+              serviceConfig = {
+                ExecStart = "/${pkgs.wartid-server}";
+                Type = "simple";
+                User = "wartid";
+                Group = "wartid";
+              };
+            };
+            systemd.services.wartid-server-discord-bot = mkIf cfg.enableDiscordBot {
+              description = "WartID server: Discord bot";
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+              environment = {
+                DISCORD_KEY_FILE = "/tmp/wartid/discord_jwt.key";
+              };
+              serviceConfig = {
+                ExecStart = "/${pkgs.wartid-server-discord-bot}";
+                Type = "simple";
+                User = "wartid";
+                Group = "wartid";
+              };
+            };
+          };
       };
     });
 }
